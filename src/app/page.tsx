81222eb429812a2469a9fd0c1c53270741de1e23
2Zerozero <hawgae@gmail.com>
2024-07-24 17:14:06 +0900

    feat: 다크 모드 테마 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,24 @@
+import { ThemeToggle } from '@/layouts/theme/ThemeToggle';
+import { Github } from 'lucide-react';
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <nav className="max-w-[1200px] h-16 flex items-center m-auto justify-between">
+        <div className="flex">2Zerozero</div>
+
+        {/* 테마 설정 및 깃허브 링크 */}
+        <div className="flex gap-4">
+          <ThemeToggle />
+          <div>
+            <Github />
+          </div>
+        </div>
+      </nav>
+
+      <div>모드 테스트</div>
+    </>
+  );
 };
 
 export default Home;
---
+++ src/lib/post.ts
@@ -0,0 +1,5 @@
+import fs from 'fs';
+import path from 'path';
+import dayjs from 'dayjs';
+import matter from 'gray-matter';
+import readingTime from 'reading-time';
