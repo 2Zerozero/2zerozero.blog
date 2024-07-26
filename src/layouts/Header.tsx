3184e5a0d742991c1445a0ec551e5d1db5132069
2Zerozero <hawgae@gmail.com>
2024-07-26 22:56:12 +0900

    feat: Tailwind 사용자 정의 스타일 설정 및 Header UI 수정

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -3,17 +3,20 @@ import { Github } from 'lucide-react';
 
 const Header = () => {
   return (
-    <nav className="max-w-[1200px] h-16 flex items-center m-auto justify-between">
-      <div className="flex">2Zerozero</div>
+    <div className="border-b-4">
+      <nav className="max-w-[1200px] h-16 flex items-center m-auto justify-between">
+        {/* 로고 */}
+        <div className="flex">2Zerozero</div>
 
-      {/* 테마 설정 및 깃허브 링크 */}
-      <div className="flex gap-4">
-        <ThemeToggle />
-        <div>
-          <Github />
+        {/* 테마 설정 및 깃허브 링크 */}
+        <div className="flex gap-4">
+          <ThemeToggle />
+          <div className="flex items-center h-10 w-10 justify-center hover:bg-muted-foreground">
+            <Github className="h-[1.2rem] w-[1.2rem]" />
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </div>
   );
 };
 
