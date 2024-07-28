1c208f6e937a2155b3d6e3154983080e2a327e44
2Zerozero <hawgae@gmail.com>
2024-07-28 17:13:34 +0900

    feat: Header 네비게이션 맵핑, 테마 사용자 경험 개선

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,23 +1,55 @@
+'use client';
+
+import Link from 'next/link';
 import { ThemeToggle } from './theme/ThemeToggle';
 import { Github } from 'lucide-react';
+import { Button } from '@/components/ui/button';
+import { cn } from '@/lib/utils';
+import { usePathname } from 'next/navigation';
+
+const navList = [
+  { name: '2Zerozero', href: '/blog' },
+  { name: 'About', href: '/about' },
+];
 
 const Header = () => {
+  const pathname = usePathname();
+
   return (
-    <div className="border-b-4">
-      <nav className="max-w-[1200px] h-16 flex items-center m-auto justify-between">
-        {/* 로고 */}
-        <div className="flex">2Zerozero</div>
+    <nav className="border-b-4 fixed w-full">
+      <div className="max-w-[1200px] h-16 flex items-center m-auto justify-between">
+        {/* 네비게이션 */}
+        <div className="flex">
+          {navList.map((navItem, index) => (
+            <Link
+              href={navItem.href}
+              key={navItem.name}
+              className={cn('mr-6', pathname?.startsWith(navItem.name) ? '' : '')}
+            >
+              {navItem.name}
+            </Link>
+          ))}
+        </div>
 
         {/* 테마 설정 및 깃허브 링크 */}
-        <div className="flex gap-4">
+        <div className="flex gap-2">
           <ThemeToggle />
-          <div className="flex items-center h-10 w-10 justify-center hover:bg-muted-foreground">
-            <Github className="h-[1.2rem] w-[1.2rem]" />
-          </div>
+          <Button asChild variant="ghost" size="icon" className="hover:bg-muted-foreground">
+            <Link href="https://github.com/2Zerozero">
+              <Github className="h-[1.2rem] w-[1.2rem]" />
+            </Link>
+          </Button>
         </div>
-      </nav>
-    </div>
+      </div>
+    </nav>
   );
 };
 
 export default Header;
+
+/*
+  pathname.startsWith()
+  JavaScript의 String.prototype.startsWith() 메서드
+
+  문자열이 특정 문자열로 시작하는지를 확인하는 구문
+*/
